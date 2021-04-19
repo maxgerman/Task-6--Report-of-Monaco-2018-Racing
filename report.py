4b33a99321661230bbd1d1d784f5477033df0cfc
Maksym German <maxjerman@gmail.com>
2021-04-19 20:25:09 +0300

    Docstrings added to module, class, functions

--- report.py
+++ report.py
@@ -1,3 +1,21 @@
+"""
+Compile the statistics of the race and return it as a pretty table. Take input logs from the files in DATA_PATH or
+any other specified path (by CLI argument --files). List all results in ascending or descending order or show only the
+specified driver (by -d, --driver command line argument).
+
+    Classes:
+
+        Driver
+
+    Functions:
+
+        build_report(data_path=DATA_PATH) -> list of drivers. Uses:
+            drivers_from_abbr(data_path) -> list of driver instances
+            parse_logs(data_path, drivers) - modifies the drivers list passed as an argument
+        print_report(drivers, asc=True, driver=None) -> str
+        parse_cli -> namespace
+"""
+
 import os
 import datetime as dt
 import argparse
@@ -10,6 +28,30 @@ END_LOG = 'end.log'
 
 
 class Driver:
+    """
+    A class to represent a driver.
+
+    Attributes
+    ----------
+    abbr : str
+        name abbreviation as in abbreviation file
+    name : str
+        driver's name
+    team : str
+        driver's team
+    start_time : datetime
+        start time of the lap
+    stop_time : datetime
+        finish time of the lap
+    best_lap : timedelta
+        time of the best lap
+
+    Methods
+    -------
+    statistics : str
+        Returns the pretty string with the driver's statistics
+    """
+
     def __init__(self, abbr, name, team, start_time=None, stop_time=None, best_lap=None):
         self.abbr = abbr
         self.name = name
@@ -26,7 +68,10 @@ class Driver:
 
 
 def drivers_from_abbr(data_path):
-    # create drivers
+    """
+    Return the list of driver instances each with their name, abbreviation and team parsed from the
+    data_path/ABBR_FILENAME
+    """
     drivers = []
     with open(os.path.join(data_path, ABBR_FILENAME), 'r', encoding='UTF-8') as f:
         for line in f:
@@ -37,7 +82,10 @@ def drivers_from_abbr(data_path):
 
 
 def parse_logs(data_path, drivers):
-    # add start times
+    """
+    Modify the passed drivers list in-place: update each with the start and finish times from the parsing of the logs
+    in data_path
+    """
     with open(os.path.join(data_path, START_LOG), 'r', encoding='UTF-8') as f:
         # use only non-blank lines
         lines = (line for line in f if line.strip())
@@ -47,7 +95,6 @@ def parse_logs(data_path, drivers):
                 if driver.abbr == abbr:
                     driver.start_time = dt.datetime.strptime(start_time, "%H:%M:%S.%f")
                     break
-    # add stop times
     with open(os.path.join(data_path, END_LOG), 'r', encoding='UTF-8') as f:
         # use only non-blank lines
         lines = (line for line in f if line.strip())
@@ -60,21 +107,28 @@ def parse_logs(data_path, drivers):
 
 
 def build_report(data_path=DATA_PATH):
-    # get drivers from abbr file
+    """
+    Build the report based on files of name abbreviations and time logs in DATA_PATH. Calculate the best lap time for
+    each driver. Return the list of drivers.
+    """
     drivers = drivers_from_abbr(data_path)
-    # add start, stop times to drivers
     parse_logs(data_path, drivers)
-    # validate times: start must be less than stop
     for driver in drivers:
         if driver.start_time > driver.stop_time:
             driver.start_time, driver.stop_time = driver.stop_time, driver.start_time
-        # calc and add best lap times
         driver.best_lap = driver.stop_time - driver.start_time
     return drivers
 
 
 def print_report(drivers, asc=True, driver=None):
-    # if requested report about one driver only
+    """
+    Return str report based on the report of the build_report function.
+
+    Parameters:
+    drivers - the list of drivers each with their info as properties
+    asc - ascending order if True
+    driver - if set, print the report of this only driver
+    """
     if driver:
         for d in drivers:
             if driver.lower() in d.name.lower():
@@ -82,11 +136,9 @@ def print_report(drivers, asc=True, driver=None):
         else:
             return 'Driver not found'
 
-    # if report about all is requested
     else:
         sorted_drivers = sorted(drivers, key=lambda dr: dr.best_lap)
         res_table = ['{:2d}. '.format(i + 1) + driver.statistics() for i, driver in enumerate(sorted_drivers)]
-        # divider after first 15 if the order is ascending (otherwise, no point)
         if asc:
             res_table.insert(15, '-' * 60)
         else:
@@ -96,6 +148,15 @@ def print_report(drivers, asc=True, driver=None):
 
 
 def parse_cli():
+    """
+    Parse CLI parameters and return the CLI namespace with parsed settings.
+
+    Parameters:
+        --files - path to the folder with the data files (with names ABBR_FILENAME, START_LOG, END_LOG)
+        --asc or --desc - order of the report
+        --driver - name or its part of the driver whose report to show
+
+    """
     parser = argparse.ArgumentParser(prog='Report of Monaco 2018', description='Drivers\' statistics')
     parser.add_argument('-f', '--files', nargs='?', default=DATA_PATH,
                         help=f'Path to data files. Default: "{DATA_PATH}"')
